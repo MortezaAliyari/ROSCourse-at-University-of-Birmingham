0f4f96f6f3a57ad6033ddc7056ee8cde53759219
Morteza Aliyari <79801785+MortezaAliyari@users.noreply.github.com>
2022-05-24 11:38:04 +0800

    Update coordinate_test.py

--- Lab4_creating_Map/src/coordinate_test.py
+++ Lab4_creating_Map/src/coordinate_test.py
@@ -1,5 +1,4 @@
-#!/usr/bin/python
-
+#!/usr/bin/python3
 from project_4.geometry import to_grid, to_world
 
 correct = 0
@@ -10,9 +9,9 @@ def grid_test(x, y, result, origin_x=0.0, origin_y=0.0, size_x=10, size_y=10, re
     
     test_coordinates = to_grid(x,y,origin_x,origin_y,size_x,size_y,resolution)
     if test_coordinates != result:
-        print 'Failed grid test: ',x,y,origin_x,origin_y,size_x,size_y,resolution
-        print '\tExpected:',result
-        print '\tReceived:',test_coordinates
+        print ('Failed grid test: ',x,y,origin_x,origin_y,size_x,size_y,resolution)
+        print ('\tExpected:',result)
+        print ('\tReceived:',test_coordinates)
     else:
         correct += 1
     total += 1        
@@ -23,9 +22,9 @@ def world_test(gx, gy, result, origin_x=0.0, origin_y=0.0, size_x=10, size_y=10,
     
     test_coordinates = to_world(gx,gy,origin_x,origin_y,size_x,size_y,resolution)
     if test_coordinates != result:
-        print 'Failed world test: ',gx,gy,origin_x,origin_y,size_x,size_y,resolution
-        print '\tExpected:',result
-        print '\tReceived:',test_coordinates
+        print ('Failed world test: ',gx,gy,origin_x,origin_y,size_x,size_y,resolution)
+        print ('\tExpected:',result)
+        print ('\tReceived:',test_coordinates)
     else:
         correct += 1
     total += 1        
@@ -57,4 +56,4 @@ world_test(0, 0, (-4.5, -4.5), origin_x=-5, origin_y=-5)
 world_test(9, 9, (4.5, 4.5), origin_x=-5, origin_y=-5)
 
 
-print "Passed %d/%d"%(correct, total)
+print ("Passed %d/%d"%(correct, total))
