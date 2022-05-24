21a5cbc2ce33d3f958fd0ec4e726600df027f69d
Morteza Aliyari <79801785+MortezaAliyari@users.noreply.github.com>
2022-05-24 11:39:12 +0800

    Update geometry.py

--- Lab4_creating_Map/src/project_4/geometry.py
+++ Lab4_creating_Map/src/project_4/geometry.py
@@ -5,14 +5,43 @@ import math
 # ------------------------------------------------------------------------------
 def to_grid(x, y, origin_x, origin_y, size_x, size_y, resolution):
 
-  return None
-    
+	if x < origin_x:
+		ans = None
+	elif y < origin_y:
+		ans = None
+	else:
+		dist_x = abs(x - origin_x)
+		gx = (dist_x/resolution)
+
+		dist_y = abs(y - origin_y)
+		gy = (dist_y / resolution)
+
+		gx = int(gx)
+		gy = int(gy)
+
+		if gx >= size_x:
+			ans = None
+		elif gy >= size_y:
+			ans = None
+		else:
+			ans = (gx,gy)
+
+	return ans
+
 # ------------------------------------------------------------------------------
 # Convert grid coordinate to world coordinate
 # ------------------------------------------------------------------------------
 def to_world(gx, gy, origin_x, origin_y, size_x, size_y, resolution):
 
-  return None
+	dist_gx = abs(origin_x - gx)
+	dist_gy = abs(origin_y - gy)
+
+	x = origin_x + ( (gx * resolution) + (resolution/2) )
+	y = origin_y + ( (gy * resolution) + (resolution/2) )
+
+	ans = (x,y)
+
+	return ans
   
 # ------------------------------------------------------------------------------
 # Convert grid coordinate to map index
@@ -68,3 +97,13 @@ def bresenham(x0, y0, x1, y1):
     if swapped:
         points.reverse()
     return points
+
+x = 4
+y = 5
+origin_x = 0
+origin_y = 0
+size_x = 20 
+size_y = 20
+resolution = 0.5
+grid_points = to_grid(x,y,origin_x,origin_y,size_x,size_y,resolution)
+print (grid_points)
